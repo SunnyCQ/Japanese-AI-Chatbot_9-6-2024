8885609dd6a6fa1c250cf39ca2426fd93e64e832
SunnyCQ <sunny02px2022@gmail.com>
2024-10-01 06:20:17 -0400

    Added LOGOUT to NavBar.

--- react_files/app/src/NavBar.jsx
+++ react_files/app/src/NavBar.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css_files/NavBar.css';  // Import a CSS file for styling
+import { auth } from '../firebase';
 
 function NavBar() {
   return (
@@ -10,7 +11,9 @@ function NavBar() {
       <Link className="nav-link" to="/TomoChat">TomoChat</Link>
       <Link className="nav-link" to="/Flashcards">Flashcards</Link>
       <Link className="nav-link" to="/About">About</Link>
+      <button onClick={() => auth.signOut()}>Logout</button>
     </nav>
+    
   );
 }
 
